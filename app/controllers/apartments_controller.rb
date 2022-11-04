613aa4b693494e2d88cb816ead7e36085d79a8b8
rhodesmt <mrhodes415@gmail.com>
2022-11-04 10:01:40 -0700

    Completed Validations Branch

--- app/controllers/apartments_controller.rb
+++ app/controllers/apartments_controller.rb
@@ -1,6 +1,6 @@
 class ApartmentsController < ApplicationController
-    def index
-        @apartments = Apartment.all
-        render json: @apartments    
-    end
+  def index
+    @apartments = Apartment.all
+    render json: @apartments
+  end
 end
